f2d0d8754ce2bb866ebc87803ba2ab98aee58bb2
SendyPradika <sendypradikaputra@gmail.com>
2024-05-06 15:20:41 +0700

    Commit project

---
+++ app/src/main/java/com/example/smarthydroponic/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.smarthydroponic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
